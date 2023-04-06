b43a7204392a3aa8c903f0b3407177ea9c7e383a
qiujingbao <qiujingbao@dlmu.edu.cn>
2023-04-06 19:00:43 +0800

    modify git

---
+++ build/CMakeFiles/linux_shell.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linux_shell.dir/main.c.o"
+  "CMakeFiles/linux_shell.dir/src/cat.c.o"
+  "CMakeFiles/linux_shell.dir/src/cd.c.o"
+  "CMakeFiles/linux_shell.dir/src/chmod.c.o"
+  "CMakeFiles/linux_shell.dir/src/date.c.o"
+  "CMakeFiles/linux_shell.dir/src/echo.c.o"
+  "CMakeFiles/linux_shell.dir/src/error.c.o"
+  "CMakeFiles/linux_shell.dir/src/exec.c.o"
+  "CMakeFiles/linux_shell.dir/src/kill.c.o"
+  "CMakeFiles/linux_shell.dir/src/ls.c.o"
+  "CMakeFiles/linux_shell.dir/src/mkdir.c.o"
+  "CMakeFiles/linux_shell.dir/src/output.c.o"
+  "CMakeFiles/linux_shell.dir/src/pwd.c.o"
+  "CMakeFiles/linux_shell.dir/src/queen.c.o"
+  "CMakeFiles/linux_shell.dir/src/rm.c.o"
+  "CMakeFiles/linux_shell.dir/src/rmdir.c.o"
+  "CMakeFiles/linux_shell.dir/src/shell_function.c.o"
+  "CMakeFiles/linux_shell.dir/src/shell_pipe.c.o"
+  "CMakeFiles/linux_shell.dir/src/string_helper.c.o"
+  "CMakeFiles/linux_shell.dir/src/wc.c.o"
+  "CMakeFiles/linux_shell.dir/src/who.c.o"
+  "CMakeFiles/linux_shell.dir/src/whoami.c.o"
+  "linux_shell"
+  "linux_shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linux_shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
